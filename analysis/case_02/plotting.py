703f3548b8d47b193ea163ef97995662a544eb01
Omaam <omama.tomoki@gmail.com>
2023-03-15 13:10:16 +0900

    add

---
+++ analysis/case_02/plotting.py
@@ -0,0 +1,126 @@
+"""Plot helpping module.
+"""
+from matplotlib.animation import ArtistAnimation
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm
+
+import diskflowsim2 as dfs2
+
+
+def plot_animation(xs, savename=None, show=False, verbose=False):
+    fig, ax = plt.subplots(figsize=(8, 5))
+    frames = []
+    vmin, vmax = np.unique(xs)[[1, -1]]  # Avoid array[0] == 0
+    for i in tqdm.trange(xs.shape[0], disable=not(verbose)):
+        frame = ax.imshow(xs[i], cmap="magma", vmax=vmax, vmin=vmin)
+        frames.append([frame])
+    ax.axis("off")
+    fig.tight_layout()
+    ani = ArtistAnimation(fig, frames, interval=100)
+    if savename is not None:
+        ani.save(savename)
+    if show:
+        plt.show()
+    plt.close()
+
+
+def plot_animation_multiple(xs, titles=None, interval=100) -> FuncAnimation:
+    """Plot animation.
+
+    Args:
+        x: shape = (num_frames, num_targets, num_layers, num_segments)
+        `num_targes` equals number of panels.
+
+    Return:
+        anim: FuncAnimation object.
+    """
+    xs = np.asarray(xs)
+    num_frames, num_panels = xs.shape[:2]
+
+    # Only show triagular areas.
+    mask = np.ones(xs.shape[-2:])
+    mask = dfs2.arrange_diskshape(mask)
+    mask = np.where(mask == 0, np.nan, 1)
+    xs = xs * mask
+
+    fig, ax = plt.subplots(1, num_panels, figsize=(6*num_panels, 6))
+    fig.tight_layout()
+
+    # Compute maximum and minimum for each panel.
+    vmaxs = np.nanmax(xs, axis=(0, 2, 3))
+    vmins = np.nanmin(xs, axis=(0, 2, 3))
+
+    def update(frame):
+        for j in range(num_panels):
+            ax[j].cla()
+            plot_snapshot(xs[frame][j], ax[j], vmaxs[j], vmins[j])
+        if titles is not None:
+            if len(titles) != num_panels:
+                raise ValueError("'len(titles)' must match 'num_panels'")
+            for j in range(num_panels):
+                ax[j].set_title(titles[j])
+        fig.suptitle(f"Frame {frame}")
+
+    anim = FuncAnimation(fig, update, frames=num_frames,
+                         interval=interval, repeat=False)
+
+    return anim
+
+
+def plot_snapshot(x, ax, vmax, vmin=0, verbose=False):
+    ax.imshow(x, cmap="magma", vmax=vmax, vmin=vmin)
+    ax.axis("off")
+    return ax
+
+
+def plot_snapshot_disk(potential, ax, verbose=False):
+    num_anulus, num_segments = potential.shape
+    r_out, r_in = num_segments, num_segments - num_anulus
+    size = (r_out - r_in) / num_anulus
+
+    cmap = plt.colormaps["magma"]
+
+    for i, anulus in enumerate(tqdm.tqdm(potential,
+                                         disable=not(verbose))):
+        radius = int(r_out - i * size)
+        anulus_true = anulus[:radius]
+        colors = cmap(anulus_true)
+        fractions = (anulus_true > 0).astype(int)
+        ax.pie(fractions, radius=radius, colors=colors,
+               wedgeprops=dict(width=size))
+    ax.set_xlim(-r_out, r_out)
+    ax.set_ylim(-r_out, r_out)
+
+    return ax
+
+
+def plot_curves(times, radiations, num_slices, savename=None, show=False):
+    num_layers = radiations.shape[1]
+
+    fig, ax = plt.subplots(num_slices + 1, sharex="col")
+    ax[0].plot(times, radiations.sum(axis=(1, 2)))
+    ax[0].set_ylabel("total")
+
+    jump = num_layers // (num_slices)
+    for i in range(num_slices):
+        curve = radiations[:, i*jump:(i+1)*jump].sum(axis=(1, 2))
+        ax[i+1].plot(times, curve)
+        ax[i+1].set_ylabel(f"layer {i}")
+    ax[-1].set_xlabel("time")
+
+    fig.align_ylabels()
+    plt.tight_layout()
+    if savename is not None:
+        plt.savefig(savename, dpi=150)
+    if show:
+        plt.show()
+
+
+def save_animation(anim, savename):
+    anim.save(savename)
+
+
+def show():
+    plt.show()
---
+++ analysis/case_02/run_simulation.py
@@ -0,0 +1,119 @@
+"""Run simulation.
+"""
+from datetime import datetime
+import os
+import sys
+
+import numpy as np
+
+import diskflowsim2 as dfs2
+import plotting
+import utils
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+def do_simulation(p_init, ws, num_iter):
+    potentials = np.zeros((num_iter, *p_init.shape))
+    radiations = np.zeros((num_iter, *p_init.shape))
+    p = p_init
+    for i in range(num_iter):
+
+        num_cells_r, num_cells_t = p.shape
+
+        p[0, 0] = np.exp(0.01 * np.random.randn(1))
+
+        r = np.arange(num_cells_t, num_cells_t - num_cells_r, -1)
+        r = np.broadcast_to(r[:, None], p.shape)
+
+        y = np.stack([p, r], axis=2)[..., None] * ws
+        y = np.sum(y, axis=2)
+        ratios = dfs2.softmax(y)
+
+        dp = p[..., None] * ratios
+
+        dp_s = dp[..., 0]
+
+        # Radial direction.
+        dp_r = dp[..., 1]
+        dp_r = dfs2.propagate_conv(dp_r)
+        dp_r[0] = 0
+
+        # Transverse direction.
+        dp_t = dp[..., 2]
+        dp_t = np.roll(dp_t, shift=1, axis=1)
+        # Insert diagonal conponent to head in layers.
+        dp_t[:, 0] = np.diag(np.fliplr(dp_t))
+
+        p = dp_s + dp_r + dp_t
+        p = np.fliplr(np.triu(np.fliplr(p)))
+
+        potentials[i] = p
+
+        radiation = dfs2.arrange_diskshape(dp[..., 3])
+        radiations[i] = radiation
+
+    return potentials, radiations
+
+
+def print_weights(names, values):
+    max_len = max([len(n) for n in names])
+    for n, v in zip(names, values):
+        v = round(v, 3)
+        print(f"{n:<{max_len + 1}}:", f"{v:>5}")
+
+
+def main():
+
+    np.random.seed(0)
+
+    p_init = np.zeros((97, 100))
+    p_init = dfs2.arrange_diskshape(p_init)
+
+    w_seed = None
+    if w_seed is None:
+        print("no w_seed is used")
+        # [dp_s, dp_r, dp_t, de]
+        w = [[+0.05, +0.20, +0.00, +0.40],  # p
+             [+0.10, +0.20, +0.25, +0.00]]  # r
+        w_seed = "NSD"
+    else:
+        print(f"use w_seed = {w_seed}")
+        with utils.change_seed_temp(seed=w_seed):
+            w = np.abs(np.random.randn(2, 4))
+            signs = np.array(
+                [[1, 1, 1, 1],
+                 [1, 1, 1, 1]]
+             )
+            w = w * signs
+            w = np.round(w, 3)
+    w /= np.sum(w)
+    np.set_printoptions(3)
+    print(w)
+
+    num_iter = 1000
+    potentials, radiations = do_simulation(p_init, w, num_iter)
+
+    now = datetime.now()
+    now_str = datetime.strftime(now, "%Y-%m-%d_%H:%M:%S")
+
+    seed_str = "seed" + str(w_seed).rjust(3, "0")
+
+    num_seqs = radiations.shape[0]
+    times = np.arange(num_seqs)
+    curvename = f"figs/curve_{seed_str}_{now_str}.png"
+    plotting.plot_curves(times, radiations, 3, show=True,
+                         savename=curvename)
+
+    xs = [[p, r] for p, r in zip(potentials, radiations)]
+    titles = ["Potential", "Radiation"]
+    anim = plotting.plot_animation_multiple(xs, titles)
+    animname = f"figs/animation_{seed_str}_{now_str}.gif"
+    plotting.save_animation(anim, animname)
+
+    now = datetime.now()
+    utils.save_info("run_info.txt", f"{seed_str:<10}", now_str, w)
+
+
+if __name__ == "__main__":
+    main()
